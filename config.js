824ab9d9faffbc8f06a16b7b43868a277f3991f8
ScottQB4 <ScottQB4@yahoo.com>
2016-09-23 10:37:05 -0400

    Update

---
+++ config.js
@@ -0,0 +1,55 @@
+var CONFIG = {
+  "maps": {
+    "World 1 Orig_isometric_day": {
+      "imageFormat": "jpg",
+      "lastRendered": [
+        1474601360,
+        1474601360,
+        1474601360,
+        1474601360
+      ],
+      "maxZoom": 8,
+      "name": "World 1 Orig - Day",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 16,
+      "tileSetGroup": "World 1 Orig_isometric_t1",
+      "tileSize": 512,
+      "world": "World 1 Orig",
+      "worldName": "World 1 Orig",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "World 1 Orig_isometric_day"
+  ],
+  "tileSetGroups": {
+    "World 1 Orig_isometric_t1": {
+      "maxZoom": 8,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
